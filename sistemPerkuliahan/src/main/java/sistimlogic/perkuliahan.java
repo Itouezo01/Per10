d38f99fff032db7717c42f2d61f3fbe65f8d9e29
A-16 <A-16@A-16>
2024-06-03 10:49:49 +0700

    membuat data

---
+++ sistemPerkuliahan/src/main/java/sistimlogic/perkuliahan.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sistimlogic;
+
+/**
+ *
+ * @author A-16
+ */
+public class perkuliahan {
+    private perkuliahan[] dataperkuliahans;
+    private Dosen[] dataDosens;
+    private mahasiswa[] datamahasiswas;
+    private staf[] datastafs;
+    
+    
+}
